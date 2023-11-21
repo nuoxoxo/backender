8d7524becdc6e7fa277e29ad47b422134d0a167f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-21 21:24:35 +0100

    + elapsed time for MOTD .

--- snl/server.cc
+++ snl/server.cc
@@ -4,10 +4,17 @@
 #include "unistd.h"
 #include "netinet/in.h"
 #include "arpa/inet.h" // inet_addr
+#include "algorithm" // shuffle
+#include "random" // default_random_engine
+#include "chrono" // system_clock
 
 #define MAX 77777
 #define BUFFSIZE 77777
 #define LOCALHOST "127.0.0.1" // 2130706433
+#define YC "\033[33m"
+#define CC "\033[36m"
+#define NC "\033[0m"
+#define INTERVAL 60
 
 std::vector<std::string> NATO = {
     "Alfa", "Bravo", "Charlie", "Delta", "Echo",
@@ -17,6 +24,13 @@ std::vector<std::string> NATO = {
     "Uniform", "Victor", "Whiskey", "X-ray", "Yankee",
     "Zulu"
 };
+
+std::string OWL = "\n     ×___×           \n\
+     (o,o)       ____\n\
+     [`\"']      /___/ \n\
+------\" \"------/____ \n\
+            \\_\\  \\__\\\n\n";
+
 
 int     sock, conn, top; 
 int     num = 0;
@@ -41,6 +55,8 @@ void    handle_consumer_input (int);
 void    handle_incoming_conn ();
 void    bind_and_listen (int);
 void    create_socket ();
+void    print_owl();
+
 
 int main(int ac, char **v)
 {
@@ -51,10 +67,13 @@ int main(int ac, char **v)
         std::cerr << "Wrong number of arguments" << std::endl;
         exit(1);
     }
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
-        drop("Dropped");
-    }
+
+    // std::random_shuffle ( NATO.begin(), NATO.end());
+    // random_shuffle was deprecated in C++14 and removed in C++17
+
+    unsigned int SEED = std::chrono::system_clock::now().time_since_epoch().count();
+    auto LAST = std::chrono::system_clock::now();
+    std::shuffle ( NATO.begin(), NATO.end(), std::default_random_engine( SEED ));
     port = atoi(v[1]);
     create_socket();
     bind_and_listen( port );
@@ -66,8 +85,17 @@ int main(int ac, char **v)
         RR = WW = AA;
         if (select(top + 1, & RR, & WW, nullptr, nullptr) < 0)
         {
-            drop("Dropped");
+            drop("Dropped: select");
         }
+
+        auto CURR = std::chrono::system_clock::now();
+        auto ELAP = std::chrono::duration_cast<std::chrono::seconds>(CURR - LAST).count();
+        if (ELAP >= INTERVAL)
+        {
+            LAST = std::chrono::system_clock::now();
+            speak(sock, OWL);
+        }
+
         int fd = -1;
         while (++fd <= top)
         {
@@ -89,11 +117,16 @@ int main(int ac, char **v)
 
 // 
 
+void print_owl()
+{
+    std::cout << OWL;
+}
+
 void create_socket ()
 {
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
-        drop("Dropped at sock");
+        drop("Dropped: sock");
     }
 }
 
@@ -106,18 +139,19 @@ void    bind_and_listen(int port)
     servaddr.sin_port = htons(port);
     if (bind(sock, (const struct sockaddr *) & servaddr, sizeof(servaddr)) == -1)
     {
-        drop("Dropped at bind");
+        drop("Dropped: bind");
     }
     else
     {
         std::cout << "bind ok \n";
         if (listen(sock, 10) == -1)
         {
-            drop("Dropped at listen");
+            drop("Dropped: listen");
         }
     }
     std::cout << "on port " << port << "\n";
-    std::cout << "listening \n\n";
+    std::cout << "listening \n";
+    print_owl();
 }
 
 void    handle_consumer_input(int fd)
@@ -125,8 +159,9 @@ void    handle_consumer_input(int fd)
 
     if ((rune = recv(fd, buff, BUFFSIZE, 0)) < 1)
     {
-        speak(fd, NATO[uuid[fd] % (int) NATO.size()] + " just left the chat\n");
-        std::cout << NATO[uuid[fd] % (int) NATO.size()] + " just left chat\n";
+        speak(fd,
+            YC + NATO[uuid[fd] % (int) NATO.size()] + " just left the chat \n" NC
+        );
         close(fd);
         inbox[fd] = "";
         FD_CLR(fd, & AA);
@@ -139,7 +174,7 @@ void    handle_consumer_input(int fd)
         inbox[fd] += buff;
         while (get_next_line(inbox[fd], s))
         {
-            speak(fd, NATO[uuid[fd]] + ": " + s);
+            speak(fd, CC + NATO[uuid[fd]] + NC + ": " + s);
         }
     }
 }
@@ -154,8 +189,8 @@ void handle_incoming_conn(void)
     top = std::max(top, conn);
     uuid[conn] = num++;
     inbox[conn] = "";
-    speak(conn, NATO[(uuid[conn])] + " just joined the chat\n");
-    std::cout << NATO[uuid[conn] % (int) NATO.size()] + " just joined the chat\n";
+    speak(conn,
+        YC + NATO[uuid[conn] % (int) NATO.size()] + " just joined the chat \n" NC);
     FD_SET(conn, & AA);
 }
 
@@ -163,6 +198,13 @@ void speak(int ff, const std::string & s)
 {
     int fd = -1;
 
+    std::cout << s;
+    /*
+    if (s != "" && s[s.size() - 1] != '\n')
+    {
+        std::cout << '\n';
+    }
+    */
     while (++fd < top + 1)
     {
         if (FD_ISSET(fd, & WW) && fd != ff)
